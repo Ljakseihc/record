3c0f8e57d3f3da3bc5ab9dd512efbf920cfd667c
ljakseihc <lyakseihc93@gmail.com>
2020-06-15 23:51:44 +0300

    init commit

---
+++ record-app/src/test/java/by/sport/record/RecordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sport.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
